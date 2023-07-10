a44d5e435cf7b6b4f092208c8e3f7fd16eb6d92d
Azza Abdalla <96057396+AzzaAbdalla@users.noreply.github.com>
2023-07-10 21:58:01 +0300

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h2 className="section-title">about us</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque nihil
+        quam laborum quis, maxime libero iste deleniti excepturi officiis,
+        accusamus id expedita amet temporibus magnam neque! Voluptates, veniam
+        perspiciatis. Consequuntur deleniti deserunt similique veniam? Magnam
+        eaque laboriosam ex velit laudantium?
+      </p>
+    </section>
+  );
+};
+
+export default About;
